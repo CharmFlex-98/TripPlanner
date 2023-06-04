b6ffa21df1bd6eee55b18e25f41e6180a1a8993c
JiaMing <lukjiaming@gmail.com>
2023-06-04 15:50:47 +0800

    created new page to add new record, not yet done. Stuck in edit text field for non-editable feature

--- app/src/main/java/com/example/tripplanner/MainActivity.kt
+++ app/src/main/java/com/example/tripplanner/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.tripplanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tripplanner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var _binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setupFAB()
+        setContentView(_binding.root)
+    }
+
+    /*
+    * Setup Fab
+    */
+    private fun setupFAB() {
+        _binding.fab.setOnClickListener {
+            AddEditTripFragment().show(supportFragmentManager, AddEditTripFragment.fragmentName)
+        }
     }
 }
